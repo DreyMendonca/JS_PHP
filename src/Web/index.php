aa087a0fe0c7c2cfe5fab36fdd4ce209a7896c36
zdeley <drey.santos2005@gmail.com>
2022-10-21 23:09:39 -0300

    php js

---
+++ src/Web/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JS PHP</title>
+</head>
+<body>
+
+<?php echo "<script>alert('CARROS ESPORTIVOS')</script>"; ?>
+<?php echo "<script src= './js/Carro.js'></script>"; ?>
+<?php echo "<script src= './js/main.js'></script>"; ?>
+
+</body>
+</html>
